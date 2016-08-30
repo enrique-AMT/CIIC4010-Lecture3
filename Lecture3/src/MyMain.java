9bbad12374ac62df9922b8782132a3a803e5bf76
enrique-AMT <enrique.marrero1@upr.edu>
2016-08-30 14:40:19 -0400

    Changed the window string to "Java Projectors", instead of "Java Gurus".

--- Lecture3/src/MyMain.java
+++ Lecture3/src/MyMain.java
@@ -11,7 +11,7 @@ public class MyMain
 		frame.add(theComponent);
 		
 		frame.setSize(300, 400);
-		frame.setTitle("Java Gurus");
+		frame.setTitle("Java Projector");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 	}
